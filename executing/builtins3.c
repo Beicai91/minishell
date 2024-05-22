f412cab1369fd90712b181b772c1b81b22c2e294
Bei Cai <bcai@c3r5s5.42lausanne.ch>
2024-05-22 18:23:39 +0200

    FINAL_EVAL

--- executing/builtins3.c
+++ executing/builtins3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins3.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eprzybyl <eprzybyl@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: bcai <bcai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 10:46:08 by bcai              #+#    #+#             */
-/*   Updated: 2024/05/22 13:01:46 by eprzybyl         ###   ########.fr       */
+/*   Updated: 2024/05/22 18:22:31 by bcai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	builtin_exit(t_cmd *cmd, t_m *m)
 	cmd_args = ((t_execcmd *)cmd)->cmd_args;
 	if (cmd_args[1] != NULL)
 		m->exit_status = ft_atoi(cmd_args[1]);
-	printf("Operation terminated\n");
 	free_tree(cmd, m);
 	lastfree_restore();
+	printf("exit\n");
 	exit(m->exit_status);
 }
 
