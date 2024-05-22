f6c6c837f2ce920727c0e7744a9c024b2240af08
Bei Cai <bcai@c3r5s5.42lausanne.ch>
2024-05-22 12:25:14 +0200

    resolved export issues. leaks to fix

--- executing/builtins2.c
+++ executing/builtins2.c
@@ -6,7 +6,7 @@
 /*   By: bcai <bcai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 10:45:50 by bcai              #+#    #+#             */
-/*   Updated: 2024/05/22 10:04:35 by bcai             ###   ########.fr       */
+/*   Updated: 2024/05/22 12:23:53 by bcai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,34 @@ void	no_value_case(char *arg)
 	}
 }
 
+void	export_all(t_m *m)
+{
+	t_list	*envcpy;
+	t_gl	*gl;
+	t_envvar	*envvars;
+	char	*temp;
+
+	gl = get_gl();
+	envvars = gl->env_vars;
+	envcpy = NULL;
+	while (envvars != NULL)
+	{
+		temp = ft_strjoin(envvars->key, "=\"");
+		temp = join_free(temp, envvars->value);
+		temp = join_free(temp, "\"");
+		build_files_list(&envcpy, temp);
+		envvars = envvars->next;
+	}
+	sort_array(&envcpy, m);
+	while (envcpy != NULL)
+	{
+		printf("declare -x %s\n", envcpy->content);
+		free(envcpy->content);
+		envcpy = envcpy->next;
+	}
+	free_t_list(&envcpy);
+}
+
 void	builtin_export(t_cmd *cmd, t_m *m)
 {
 	char	**cmd_args;
@@ -42,7 +70,8 @@ void	builtin_export(t_cmd *cmd, t_m *m)
 
 	cmd_args = ((t_execcmd *)cmd)->cmd_args;
 	if (cmd_args[1] == NULL)
-		builtin_error(cmd, m, "Expected argument to \"export\"");
+		export_all(m);
+		//builtin_error(cmd, m, "Expected argument to \"export\"");
 	i = 0;
 	while (cmd_args[++i] != NULL)
 	{
